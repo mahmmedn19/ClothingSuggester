fb804e29b35624e9d13324c4aeac15dc9a850af6
Mohamed Naser <mahmmedn19@gmail.com>
2023-04-08 15:00:22 +0200

    add required libireires and secure api_key

--- app/src/main/java/com/example/clothingsuggester/MainActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val api_key = BuildConfig.API_KEY
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
