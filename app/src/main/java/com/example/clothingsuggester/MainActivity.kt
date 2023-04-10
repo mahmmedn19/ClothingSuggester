823343f31d156bc2c1abe4a074035ad055d30568
Mohamed Naser <mahmmedn19@gmail.com>
2023-04-10 22:32:37 +0200

    create data class and add OkHttpClient

--- app/src/main/java/com/example/clothingsuggester/MainActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.clothingsuggester
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.viewbinding.BuildConfig
+import androidx.appcompat.app.AppCompatActivity
+import com.example.clothingsuggester.data.Location
+import com.example.clothingsuggester.databinding.ActivityMainBinding
+import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     //val api_key = BuildConfig
     val client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         makeRequest()
 
@@ -20,18 +25,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeRequest() {
-        Log.i("Main","Make")
-        val request = Request.Builder().url("https://api.weatherapi.com/v1/current.json?key=8454ad4865ef4dc28a9195902230904&q=Tanta")
+        Log.i(TAG, "Make")
+
+        val url = HttpUrl.Builder()
+            .scheme(SCHEME)
+            .host(BASE_URL)
+            .addQueryParameter(WEATHER_API_KEY, "123")
+            .addQueryParameter(WEATHER_Q, "egypt")
             .build()
-        client.newCall(request).enqueue(object :Callback{
+
+        val request = Request.Builder()
+            .url(url).build()
+
+        client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                Log.i("Main","${e.message}")
+                Log.i(TAG, "${e.message}")
             }
 
             override fun onResponse(call: Call, response: Response) {
-                Log.i("Main",response.body?.string().toString())
+                response.body?.string()?.let {
+                    val result = Gson().fromJson(it, Location::class.java)
+                    runOnUiThread {
+                        binding.textDate.text = result.country
+                    }
+                }
+                Log.i(TAG, response.body?.string().toString())
             }
 
         })
+    }
+
+    companion object {
+        const val TAG = "Main"
+        const val SCHEME = "https"
+        const val BASE_URL = "api.weatherapi.com/v1/current.json"
+
+        //  const val WEATHER_API_KEY = BuildConfig.
+        const val WEATHER_API_KEY = "key"
+        const val WEATHER_Q = "q"
     }
 }
---
+++ app/src/main/java/com/example/clothingsuggester/data/WeatherData.kt
@@ -0,0 +1,6 @@
+package com.example.clothingsuggester.data
+
+data class WeatherData(
+    val current: Current,
+    val location: Location
+)
